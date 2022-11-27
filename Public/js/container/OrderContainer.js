411440388bc2ab17cc70dd9484e3a967b1f449c9
MarianoCarrizo <warlockphantasy@gmail.com>
2022-11-27 03:04:48 -0300

    misery

--- Public/js/container/OrderContainer.js
+++ Public/js/container/OrderContainer.js
@@ -2,16 +2,11 @@ import { Order } from "../components/Order.js";
 import { getOrdenes } from "../services/fetchServices.js";
 
 const renderOrdenes = (json) =>{
+    
     let _root = document.getElementById("ordenes-content");
+    _root.innerHTML = "";
     Object.values(json).forEach(orden => {
         _root.innerHTML+=Order(orden.numero=+1,orden.fecha,orden.total);
-    });
-}
-const renderOrdenesVacio = (json) =>{
-    let _root = document.getElementById("ordenes-content");
-    Object.values(json).forEach(orden => {
-        _root.innerHTML+=Order(orden.numero=+1,orden.fecha,orden.total);
-        _root.innerHTML = "";
     });
 }
 
