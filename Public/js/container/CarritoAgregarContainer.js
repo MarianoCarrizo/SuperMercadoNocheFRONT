2596d8133a85cd90b9e1fc1e2ffa80342c9196b9
MarianoCarrizo <warlockphantasy@gmail.com>
2022-12-01 17:28:28 -0300

    fin

--- Public/js/container/CarritoAgregarContainer.js
+++ Public/js/container/CarritoAgregarContainer.js
@@ -1,10 +1,13 @@
 import { productoAgregado } from "../components/productoAgregado.js";
 import { AddProduct } from "../services/fetchServices.js";
-
+import { vacio } from "../components/vacio.js";
 const renderAgregado = (json) =>{
     
 let _root = document.getElementById("producto-index");
+let xd = document.getElementById("borrarForm");
 _root.innerHTML=productoAgregado("")
+xd.innerHTML=vacio("");
+
 }
 
 
