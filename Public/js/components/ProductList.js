7bb735a02e2365f5e96f1d8ee919c9ccb6808400
MarianoCarrizo <mariano.carrizo4280@gmail.com>
2024-11-14 21:36:03 -0300

    Modified more front views; working on ListaProductosVista

--- Public/js/components/ProductList.js
+++ Public/js/components/ProductList.js
@@ -1,28 +1,21 @@
-export const Producto = (nombre,precio,image,marca,productoId,descripcion) => `
-<div class="col-md-12">
-            
-<div class="container card card-body">
-            <div class="media text-lg-left  flex-lg-row">
-                <div class="">
-                    
-                        <img src="${image}" width="150" height="150" alt="">
-                   
-                </div>
-                <div class="media-body">
-                    <h2 class="media-title font-weight-semibold">
-                        <a href="../view/AgregarProductoSingular.html?productoId=${productoId}" data-abc="true">${nombre} ${marca}</a>
-                    </h2>
+export const Producto = (nombre, image, marca, productoId) => `
+<div class="card-container">
+  <div class="card">
+    <div class="card-body">
+      <div class="card-img-actions">
+        <img id="img1" class="card-img img-fluid" alt="" src="${image}">
+      </div>
+    </div>
+    <div class="card-body bg-light text-center">
+      <div class="mb-2">
+        <h6 class="font-weight-semibold mb-2">
+          <a id="nombre1" href="../view/AgregarProductoSingular.html?productoId=${productoId}" class="text-default mb-" data-abc="true">
+            ${nombre} ${marca}
+          </a>
+        </h6>
+      </div>
+    </div>
+  </div>
+</div>
+`;
 
-                    ${descripcion}
-                </div>
-
-                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
-                    <h3 class="mb-0 font-weight-semibold">$${precio}</h3>
-
-                    <form>
-			<a href="../view/AgregarProductoSingular.html?productoId=${productoId}"<button Class="bttn">Agregar al Carrito</button></a>
-			</form>
-                </div>
-            </div>
-        </div>
-`
--- Public/js/container/ProductList.js
+++ Public/js/container/ProductList.js
@@ -1,16 +1,14 @@
 import { Producto } from "../components/ProductList.js";
 import { getProductos } from "../services/fetchServices.js";
 
-
-const renderProductos = (json) =>{
-
-    let _root = document.getElementById("producto-lista");
-    Object.values(json).forEach(producto => {
-        _root.innerHTML+=Producto(producto.nombre, producto.precio, producto.image,producto.marca,producto.productoId,producto.descripcion);
-    });
-
-}
+const renderProductos = (json) => {
+  let _root = document.getElementById("producto-lista");
+  _root.innerHTML = '';  // Clear previous content
+  Object.values(json).forEach(producto => {
+    _root.innerHTML += Producto(producto.nombre, producto.image, producto.marca, producto.productoId);
+  });
+};
 
 export const ProductosRender = () => {
-    getProductos(renderProductos);
+  getProductos(renderProductos);
 }
